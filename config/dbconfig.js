a24ceec0cdc8818b0d86c1af67bd83b2515e53f0
KSB-tqk <linh05602@gmail.com>
2021-10-02 19:59:18 +0700

    initial commit

---
+++ config/dbconfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: 'dbsecret',
+    database: 'mongodb+srv://admin:admin123@cluster0.wlagm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const action = require('../methods/actions')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+router.get('/dashboard', (req, res) => {
+    res.send('Dashboard')
+})
+
+//add new user
+//route POST /adduser
+router.post('/adduser', action.addNew)
+//authenticate a user
+//route POST /authenticate
+router.post('/authenticate', action.authenticate)
+//get info a user
+//route GET /getinfo 
+router.get('/getinfo', action.getinfo)
+
+module.exports = router
