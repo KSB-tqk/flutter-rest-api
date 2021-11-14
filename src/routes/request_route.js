96f29d91965e0cdc3857660cc7ded708ea423fc2
Nguyễn Thành Hiếu <19521505@gm.uit.edu.vn>
2021-11-14 20:59:14 +0700

    update - get all request

--- src/routes/request_route.js
+++ src/routes/request_route.js
@@ -2,6 +2,8 @@ const express = require('express')
 const request_method = require('../controllers/request_controller')
 const router = express.Router()
 
+
+router.get('/', request_method.getAllRequest)
 //add new request 
 //route POST /add
 router.post('/add', request_method.addNew)
