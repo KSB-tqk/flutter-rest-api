2505566bd82b4e58eb5273741e48d85f3dcc2c1e
Nguyễn Thành Hiếu <19521505@gm.uit.edu.vn>
2021-11-22 10:36:29 +0700

    update- delete booking

--- src/controllers/room_controller.js
+++ src/controllers/room_controller.js
@@ -24,11 +24,16 @@ const roomService = {
             console.log(e)
         }
     },
-    insertBooked: async function(req, res){
+    
+    
+}
+
+ const bookingService = {
+    insertBooking: async function(req, res){
         const {id} = req.params;
 
         if(!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({msg: `No request Request with id: ${id}`});
+            return res.status(404).json({msg: `No Room with id: ${id}`});
         }
         try{
             const room = await Room.findById(id);
@@ -41,7 +46,33 @@ const roomService = {
         } catch (err){
             res.status(409).json({ msg: err.message });
         }
+    },
+    deleteBooking: async function(req, res){
+        const {id} = req.params;
+        const {reservationId} = req.body;
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({msg: `No Room with id: ${id} `});
+            
+        }
+        if(!mongoose.Types.ObjectId.isValid(reservationId)){
+            return res.status(404).json({msg: `No Booking with id: ${reservationId} `});
+        }
+        try{
+            const room = await Room.findById(id);
+            await room.booked.pull(reservationId);
+            try{
+                const reservation = await reservationRoom.findByIdAndRemove(reservationId);
+                await reservation.save();
+            } catch (e){
+                console.log(e.message);
+            }
+            await room.save();
+            res.status(404).json({msg: "Delete booking success"});
+        } catch(err){
+            res.status(409).json({msg: err.message});
+        }
     }
 }
 
-module.exports = roomService
+module.exports = {roomService, bookingService}
