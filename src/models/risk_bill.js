aabaf0f18e2d8df36b5354784887979996483edd
KSB-tqk <linh05602@gmail.com>
2021-12-28 10:19:54 +0700

    fix bug typo in risk bill model

--- src/models/risk_bill.js
+++ src/models/risk_bill.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var riskBillSchema = new Schema({
-    troubleName: {
+    riskName: {
         type: String,
         require: true
     },
